55cf8c3ab11c6b951da448ea9a1af90cf57a4c46
ViniHGV <ixxvinicius@gmail.com>
2023-12-10 17:05:12 -0300

    feat: redefinindo app component e appmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SwiperComponent } from './components/swiper/swiper.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GetDatesSwiperService } from './services/get-dates-swiper.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [GetDatesSwiperService],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SwiperComponent,
+    HttpClientModule,
+    HeaderComponent,
+  ],
 })
 export class AppComponent {
   title = 'blog-challenge';
