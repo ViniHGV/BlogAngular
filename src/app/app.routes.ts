402d937a68645d493866572cc45c445fbd58b9f5
ViniHGV <ixxvinicius@gmail.com>
2023-12-17 22:51:10 -0300

    feat: criando roteamento do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.component';
+import { NewDetailComponent } from './pages/new-detail/new-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'new/:id', component: NewDetailComponent },
+];
