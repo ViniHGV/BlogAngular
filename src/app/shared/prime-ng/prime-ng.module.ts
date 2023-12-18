666c675704e06b6f09d722b86a8f40a47b843bfd
ViniHGV <ixxvinicius@gmail.com>
2023-12-17 22:53:04 -0300

    feat: criando module de primeNG

---
+++ src/app/shared/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SkeletonModule } from 'primeng/skeleton';
+
+@NgModule({
+  declarations: [],
+  exports: [SkeletonModule],
+  imports: [CommonModule, SkeletonModule],
+})
+export class PrimeNGModule {}
